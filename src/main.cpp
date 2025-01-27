3803a59e81c9b93567abd63a7d4470c5965defca
TheExplosion3 <gowl2515@gmail.com>
2025-01-27 12:54:07 -0500

    update comments

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,16 @@ void setup()
 // repeats whats assigned to be done here over and over again
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
+  // turn the LED on (HIGH is the voltage level, meaning 1)
   digitalWrite(LED_BUILTIN, HIGH);
 
   // wait for a second
   delay(1000);
 
-  // turn the LED off by making the voltage LOW
+  // turn the LED off by making the voltage LOW (0)
   digitalWrite(LED_BUILTIN, LOW);
 
    // wait for a second
   delay(1000);
+
 }
