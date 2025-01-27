7f939a0a33989d0b0e579b6822548de739364d92
TheExplosion3 <gowl2515@gmail.com>
2025-01-27 13:26:11 -0500

    formatting and new fadein method for LED

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,13 @@
 #endif
 
 // defines the setup for pins and etc?
-void setup()
-{
+void setup() {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // repeats whats assigned to be done here over and over again
-void loop()
-{
+void loop() {
   // turn the LED on (HIGH is the voltage level, meaning 1)
   digitalWrite(LED_BUILTIN, HIGH);
 
@@ -31,5 +29,19 @@ void loop()
 
    // wait for a second
   delay(1000);
+}
 
+static void fadein () {
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(30);
 }
